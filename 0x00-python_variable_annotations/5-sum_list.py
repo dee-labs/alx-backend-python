b86a2c86305d1914e7079a361ca71bc5ac0d9f70
Gemma-Mariae <divinechisom1995@gmail.com>
2022-04-29 04:28:47 +0100

    :label: added float to the return value

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -8,5 +8,5 @@ from typing import List
 def sum_list(input_list: List[float]) -> float:
     """ Computes the sum of a list of floating-point numbers.
     """
-    return sum(input_list)
+    return float(sum(input_list))
     
