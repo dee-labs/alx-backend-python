7e2365a42de4ceffa07842c99705bcc6e7213969
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-02 16:40:55 +0100

    :bug: fixed pep8 style issue

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Executes wait_random n times.
     """
-    delay_time = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delay_time = [asyncio.create_task(wait_random(max_delay))
+                  for _ in range(n)]
     return [await task for task in asyncio.as_completed(delay_time)]
-    
