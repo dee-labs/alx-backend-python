bcfccc61ca11dca04e2bcc0ed2646c28589c6857
Gemma-Mariae <divinechisom1995@gmail.com>
2022-05-02 10:24:29 +0100

    :bug: fixed pep8 style issue

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-"""0. The basics of async
-"""
-
+"""0. The basics of async"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Waits for a random number of seconds.
